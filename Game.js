eb6d2f68db2e68fbfdd30da996c20e39e7ac144a
Saidaman <68653877+Saidaman@users.noreply.github.com>
2022-05-16 12:12:20 -0400

    Game constructor commit

--- Game.js
+++ Game.js
@@ -3,7 +3,18 @@ import Ship from "./model/Ship.js";
 import "./style.css";
 
 class Game {
-
+    constructor(ctx, canvas) {
+        this.ctx = ctx;
+        this.canvas = canvas;
+        this.gameStart = false;
+        this.ship = new Ship(215, 540, 50, 50, "#00FFFF", this.gameStart);
+        this.gameOverFlag = false;
+        this.score = 0;
+        this.audio = new Audio("./assets/music.mpeg");
+        this.audio.pause();
+        this.invadersInPlay = [];
+    }
+    
 }
 
 export default Game;
